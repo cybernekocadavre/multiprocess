a1e31000a51d3e0cc33fbbfc45fa91342b8d0b19
cybernekocadavre <124934209+cybernekocadavre@users.noreply.github.com>
2024-04-19 14:53:46 +0300

    Update multi.py

--- multi.py
+++ multi.py
@@ -3,24 +3,21 @@
 
 # In[ ]:
 
-
-from multiprocessing import Process, Pool
+from multiprocessing import Process, Pool, Array
 
 def calculate_element(i, j, matrix1, matrix2, result, rows):
     # Подсчёт элементов в матрице-результате
-    
     res = 0
     for k in range(rows):
         res += matrix1[i][k] * matrix2[k][j]
-    result[i][j] = res
+    result[i * len(matrix2[0]) + j] = res
 
 def multiply_matrices(matrix1, matrix2, result, num_processes):
-    #Умножение матриц с мультипроцессорными вычислениями
+    # Умножение матриц с мультипроцессорными вычислениями
     rows = len(matrix1)
     cols = len(matrix2[0])
 
     processes = []
-
     for i in range(rows):
         for j in range(cols):
             process = Process(target=calculate_element, args=(i, j, matrix1, matrix2, result, len(matrix2)))
@@ -30,30 +27,18 @@ def multiply_matrices(matrix1, matrix2, result, num_processes):
     for process in processes:
         process.join()
 
-
-def write_matrix_to_file(matrix, filename):
-    
-    with open(filename, 'w') as file:
-        for row in matrix:
-            file.write(' '.join(map(str, row)) + '\n')
-
 if __name__ == "__main__":
-    
     matrix1 = [[1, 2], [3, 4]]
     matrix2 = [[2, 0], [1, 2]]
 
-    #Создание результирующей матрицы
-    result = [[0 for _ in range(len(matrix2[0]))] for _ in range(len(matrix1))]
+    # Создание результирующей матрицы
+    result = Array('i', [0] * len(matrix1) * len(matrix2[0]))
 
-    #Процесс умножения
-    num_processes = 4  #Тут можно менять по своему желанию
+    # Процесс умножения
+    num_processes = 4  # Тут можно менять по своему желанию
     multiply_matrices(matrix1, matrix2, result, num_processes)
-    
-    write_matrix_to_file(result, "result_matrix.txt")
 
+    # Преобразование результата в матрицу
+    result_matrix = [[result[i * len(matrix2[0]) + j] for j in range(len(matrix2[0]))] for i in range(len(matrix1))]
 
-# In[ ]:
-
-
-
-
+    print(result_matrix)
